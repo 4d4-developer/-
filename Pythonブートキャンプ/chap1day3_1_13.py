a9a0e6a5d25a22f0b2340402bbb54c43ed45b124
book_engineer <yuto08032007@outlook.jp>
2026-09-10 02:19:47 +0900

    lambdaが難しい

---
+++ Pythonブートキャンプ/chap1day3_1_13.py
@@ -0,0 +1,5 @@
+def calculate_average(my_list):
+    return sum(my_list)/len(my_list)
+my_list =  [10,20,30,40,50]
+output=calculate_average(my_list)
+print(output)
---
+++ Pythonブートキャンプ/chap1day3_1_14.py
@@ -0,0 +1,4 @@
+calculate_average = lambda numbers:sum(numbers)/len(numbers)
+my_list = [10,20,30,40,50]
+avg = calculate_average(my_list)
+print(avg)
